16ddf45ae3512e988d14b866faa7b94b7f793aee
nurulcse7 <nurul.cse7@gmail.com>
2022-12-23 20:50:42 +0600

    All category added

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const express = require("express");
-const cors = require("cors");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("Resale Furniture server is running now ");
+app.get('/', (req, res) => {
+  res.send('Resale Furniture server is running now ');
 });
 app.listen(port, () => {
-	console.log("resale furniture port is running", port);
+  console.log('resale furniture port is running', port);
 });
 
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.70yiu6o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+async function run() {
+	const furnitureCollections = client.db('furnitureCollection').collection('furnitures')
+
+	app.get('/categories', async (req, res) => {
+		const query = {}
+		const result = await furnitureCollections.find(query).toArray()
+		res.send(result)
+	})
+}
+run().catch(err => {
+	console.log(err);
+});
